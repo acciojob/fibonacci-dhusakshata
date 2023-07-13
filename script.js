825e302cceff3b1df2d5ab722d2337a09b9f32d3
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-07-13 10:44:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function fibonacci(num) {
 // your code here
+	
+	
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  } else {
+    let fibPrev = 0;  // Previous Fibonacci number
+    let fibCurrent = 1;  // Current Fibonacci number
+
+    for (let i = 2; i <= num; i++) {
+      let nextFib = fibPrev + fibCurrent;
+      fibPrev = fibCurrent;
+      fibCurrent = nextFib;
+    }
+    return fibCurrent;
+  }
 }
 
 module.exports = fibonacci;
