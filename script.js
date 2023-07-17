4eb45d1c6fd47639a0d1c28a9848596aa732b5eb
dhusakshata <130928825+dhusakshata@users.noreply.github.com>
2023-07-17 18:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function fibonacci(num) {
-  if (num < 0) {
-    return   "Invalid input. Please enter a non-negative integer." ;
-  } else if (num === 1) {
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) 
     return 1;
   }
 
